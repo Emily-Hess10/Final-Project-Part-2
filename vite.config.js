13c73b638187a6b7a7c795ff0ed1df534e73bf72
Emily Hess <erhess@live.carlow.edu>
2026-03-04 16:58:16 -0500

    Add Vite config and redirects for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: "./src/setupTests.js"
+  build: {
+    outDir: 'dist'
   }
-})
+});
